739e40c115313cf3276738a7733142d273f8e32d
Kasim Sumra <kasimsumra07@gmail.com>
2022-07-19 12:26:58 +0530

    Task works and storage part done

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mygoals/app/data/services/storage/services.dart';
 
-import 'screens/home_page.dart';
+import 'app/modules/home/view.dart';
 
-void main() {
+
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-      ),
-      home: const HomePage(),
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo List using GetX',
+      home: HomePage(),
     );
   }
 }
